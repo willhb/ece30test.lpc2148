4b567ab1cb14c0688b93d9f5bae612da235554ec
William Breidenthal <william@ke6xo.com>
2013-06-16 22:40:08 -0700

    Added code
    It works!

---
+++ .dep/main.o.d
@@ -0,0 +1,58 @@
+main.o: main.c \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/stdio.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/_ansi.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/newlib.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/config.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/features.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/include/stddef.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/include/stdarg.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/reent.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/_ansi.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/_types.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/machine/_types.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/machine/_default_types.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/lock.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/types.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/machine/types.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/stdio.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/lpc2148/lpc214x.h \
+ /Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/lpc2148/openlpc.h
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/stdio.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/_ansi.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/newlib.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/config.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/machine/ieeefp.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/features.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/include/stddef.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/include/stdarg.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/reent.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/_ansi.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/_types.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/machine/_types.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/machine/_default_types.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/lock.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/types.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/machine/types.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/sys/stdio.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/lpc2148/lpc214x.h:
+
+/Users/william/Applications/openlpc/bin/../lib/gcc/arm-none-eabi/4.5.2/../../../../arm-none-eabi/include/lpc2148/openlpc.h:
